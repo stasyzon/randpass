3b88d8ca6b1cab8c2be0e79c2274df925295d896
Stanislav <s.sizonenko@adkernel.com>
2023-09-10 20:25:05 +0300

    Add Google Ads script to RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9980612679743872"
+            crossOrigin="anonymous"></script>
       <Analytics/>
       <body className={inter.className}>{children}</body>
     </html>
