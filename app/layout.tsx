ef7fc72e3f09a502d8fabef061f1a7614ed852d0
Stanislav <s.sizonenko@adkernel.com>
2023-09-10 16:12:20 +0300

    Add Vercel Analytics to layout and package.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Analytics/>
       <body className={inter.className}>{children}</body>
     </html>
   )
