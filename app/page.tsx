4a31fd9fe78d49e8396d49dbb3a963c4b5e9d01b
Stanislav <s.sizonenko@adkernel.com>
2023-09-11 22:53:12 +0300

    Refactor imports and update text content

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import {useForm} from "react-hook-form"
-import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card"
+import {Card} from "@/components/ui/card"
 import {Label} from "@/components/ui/label"
 import {Slider} from "@/components/ui/slider"
 import * as z from "zod"
@@ -12,13 +12,11 @@ import {
   Form,
   FormControl,
   FormField,
-  FormItem,
-  FormMessage,
+  FormItem
 } from "@/components/ui/form"
 import CardWithSlider from "@/components/cardWithSwitcher";
 import {zodResolver} from "@hookform/resolvers/zod";
 import PasswordInput from "@/components/passwordInput";
-import {useCopyToClipboard} from "usehooks-ts";
 import {Separator} from "@/components/ui/separator"
 
 const formSchema = z.object({
@@ -208,19 +206,19 @@ function InputForm() {
       <Card className="w-full p-5 space-y-5">
         <div className="space-y-4">
           <h4 className="font-semibold tracking-tight text-md">RandPass: Next-Gen Password Protection</h4>
-          <p className="text-sm">Welcome to RandPass, where modern technology meets everyday security needs. In an online world that's
-            always evolving, so are the threats. That's where we come in. With just a few taps, RandPass crafts
-            passwords that aren’t just tough—they're virtually unbeatable. Whether you're keeping your financial data
+          <p className="text-sm">Welcome to RandPass, where modern technology meets everyday security needs. In an online world that&apos;s
+            always evolving, so are the threats. That&apos;s where we come in. With just a few taps, RandPass crafts
+            passwords that aren’t just tough—they&apos;re virtually unbeatable. Whether you&apos;re keeping your financial data
             secure, guarding your emails, or just locking down your social media, RandPass is the modern choice for a
             digital-first world.</p>
         </div>
         <Separator/>
         <div className="space-y-4">
           <h4 className="font-semibold tracking-tight text-md">Beyond Just Passwords: The RandPass Difference</h4>
-          <p className="text-sm">You've used password generators, but not like this. RandPass isn't just about creating a string of
-            characters; it's about giving you the reins. Want longer, trickier, or a specific set of characters? No
-            problem. Our state-of-the-art tech doesn't just follow standards; it sets them. And with our commitment to
-            generating passwords that stand strong against both brute-force and dictionary attacks, you're always one
+          <p className="text-sm">You&apos;ve used password generators, but not like this. RandPass isn&apos;t just about creating a string of
+            characters; it&apos;s about giving you the reins. Want longer, trickier, or a specific set of characters? No
+            problem. Our state-of-the-art tech doesn&apos;t just follow standards; it sets them. And with our commitment to
+            generating passwords that stand strong against both brute-force and dictionary attacks, you&apos;re always one
             step ahead. Dive into the future of security with RandPass.</p>
         </div>
       </Card>
