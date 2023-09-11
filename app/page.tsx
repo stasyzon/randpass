fd24c9d5875309690fb88d6c7e1cbfba409f13e5
Stanislav <s.sizonenko@adkernel.com>
2023-09-11 22:46:07 +0300

    Update UI layout and add content to password generator

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 "use client"
 
 import {useForm} from "react-hook-form"
-import {Card} from "@/components/ui/card"
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card"
 import {Label} from "@/components/ui/label"
 import {Slider} from "@/components/ui/slider"
 import * as z from "zod"
 import {generatePassword} from "@/lib/generate"
+import Image from 'next/image'
 
 import {
   Form,
@@ -18,6 +19,7 @@ import CardWithSlider from "@/components/cardWithSwitcher";
 import {zodResolver} from "@hookform/resolvers/zod";
 import PasswordInput from "@/components/passwordInput";
 import {useCopyToClipboard} from "usehooks-ts";
+import {Separator} from "@/components/ui/separator"
 
 const formSchema = z.object({
   generatedPassword: z.string().optional(),
@@ -68,9 +70,9 @@ function InputForm() {
   }
 
   return (
-    <div className="container max-w-screen-md mx-auto h-screen py-6">
+    <div className="flex flex-col items-center container max-w-screen-md mx-auto h-screen py-6 space-y-8">
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 w-full">
           <FormField
             control={form.control}
             name="generatedPassword"
@@ -106,88 +108,122 @@ function InputForm() {
               </FormItem>
             )}
           />
-          <FormField
-            control={form.control}
-            name="isIncludeSymbols"
-            render={({field}) => (
-              <FormItem>
-                <FormControl>
-                  <CardWithSlider
-                    onCheckedChange={field.onChange}
-                    checked={field.value}
-                    label="Include symbols"
-                    description="e.g. !@#$%^&*()+_\-=}{[\]|:;/?.><,`~]"
-                  />
-                </FormControl>
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="isIncludeNumbers"
-            render={({field}) => (
-              <FormItem>
-                <FormControl>
-                  <CardWithSlider
-                    onCheckedChange={field.onChange}
-                    checked={field.value}
-                    label="Include numbers"
-                    description="e.g. 123456"
-                  />
-                </FormControl>
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="isIncludeLowercase"
-            render={({field}) => (
-              <FormItem>
-                <FormControl>
-                  <CardWithSlider
-                    onCheckedChange={field.onChange}
-                    checked={field.value}
-                    label="Include lowercase"
-                    description="e.g. abcdefgh"
-                  />
-                </FormControl>
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="isIncludeUppercase"
-            render={({field}) => (
-              <FormItem>
-                <FormControl>
-                  <CardWithSlider
-                    onCheckedChange={field.onChange}
-                    checked={field.value}
-                    label="Include uppercase"
-                    description="e.g. ABCDEFGH"
-                  />
-                </FormControl>
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="excludeSimilar"
-            render={({field}) => (
-              <FormItem>
-                <FormControl>
-                  <CardWithSlider
-                    onCheckedChange={field.onChange}
-                    checked={field.value}
-                    label="Exclude similar characters"
-                    description="e.g. i, l, 1, L, o, 0, O"
-                  />
-                </FormControl>
-              </FormItem>
-            )}
-          />
+          <div className="flex flex-row w-full space-x-4">
+            <FormField
+              control={form.control}
+              name="isIncludeSymbols"
+              render={({field}) => (
+                <FormItem className="w-full">
+                  <FormControl>
+                    <CardWithSlider
+                      onCheckedChange={field.onChange}
+                      checked={field.value}
+                      label="Include symbols"
+                      description="e.g. !@#$%^&*()+_\-=}{[\]|:;/?.><,`~]"
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="isIncludeNumbers"
+              render={({field}) => (
+                <FormItem className="w-full">
+                  <FormControl>
+                    <CardWithSlider
+                      onCheckedChange={field.onChange}
+                      checked={field.value}
+                      label="Include numbers"
+                      description="e.g. 123456"
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="flex flex-row w-full space-x-4">
+            <FormField
+              control={form.control}
+              name="isIncludeLowercase"
+              render={({field}) => (
+                <FormItem className="w-full">
+                  <FormControl>
+                    <CardWithSlider
+                      onCheckedChange={field.onChange}
+                      checked={field.value}
+                      label="Include lowercase"
+                      description="e.g. abcdefgh"
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="isIncludeUppercase"
+              render={({field}) => (
+                <FormItem className="w-full">
+                  <FormControl>
+                    <CardWithSlider
+                      onCheckedChange={field.onChange}
+                      checked={field.value}
+                      label="Include uppercase"
+                      description="e.g. ABCDEFGH"
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="flex flex-row w-full space-x-4">
+            <FormField
+              control={form.control}
+              name="excludeSimilar"
+              render={({field}) => (
+                <FormItem className="w-full">
+                  <FormControl>
+                    <CardWithSlider
+                      onCheckedChange={field.onChange}
+                      checked={field.value}
+                      label="Exclude similar characters"
+                      description="e.g. i, l, 1, L, o, 0, O"
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            {/*<Card className="w-full flex justify-center items-center">*/}
+            {/*  <span className="text-2xl font-medium">Donate</span>*/}
+            {/*</Card>*/}
+          </div>
         </form>
       </Form>
+      <Image
+        src="/support.png"
+        width={468}
+        height={60}
+        alt="Picture of the author"
+      />
+      <Card className="w-full p-5 space-y-5">
+        <div className="space-y-4">
+          <h4 className="font-semibold tracking-tight text-md">RandPass: Next-Gen Password Protection</h4>
+          <p className="text-sm">Welcome to RandPass, where modern technology meets everyday security needs. In an online world that's
+            always evolving, so are the threats. That's where we come in. With just a few taps, RandPass crafts
+            passwords that aren’t just tough—they're virtually unbeatable. Whether you're keeping your financial data
+            secure, guarding your emails, or just locking down your social media, RandPass is the modern choice for a
+            digital-first world.</p>
+        </div>
+        <Separator/>
+        <div className="space-y-4">
+          <h4 className="font-semibold tracking-tight text-md">Beyond Just Passwords: The RandPass Difference</h4>
+          <p className="text-sm">You've used password generators, but not like this. RandPass isn't just about creating a string of
+            characters; it's about giving you the reins. Want longer, trickier, or a specific set of characters? No
+            problem. Our state-of-the-art tech doesn't just follow standards; it sets them. And with our commitment to
+            generating passwords that stand strong against both brute-force and dictionary attacks, you're always one
+            step ahead. Dive into the future of security with RandPass.</p>
+        </div>
+      </Card>
     </div>
   )
 }
--- components/passwordInput.tsx
+++ components/passwordInput.tsx
@@ -6,18 +6,27 @@ import {useCopyToClipboard} from "usehooks-ts";
 function PasswordInput({generateDisabled, value, submit}: any) {
   const [, copy] = useCopyToClipboard()
 
-  return (
-    <Card className="p-4 w-full flex flex-row justify-between items-center">
-      <div></div>
-      <span className="text-3xl font-mono">{value || 'Will be here...'}</span>
+  if (!value) {
+    return (
+      <Card className="px-4 py-10 w-full flex flex-row justify-center items-center">
+        <Button className="h-10" variant="default" onClick={() => submit()}>
+          Generate
+        </Button>
+      </Card>
+    )
+  }
 
-      <div className="flex flex-col items-end space-y-2">
+  return (
+    <Card className="px-4 py-10 w-full flex flex-row justify-center items-center relative">
+      <span className="text-3xl font-mono h-10">{value}</span>
+
+      <div className="flex flex-col items-end space-y-2 absolute right-4">
         <Button disabled={generateDisabled} variant="outline" size="icon" className="border-0 bg-muted">
           <UpdateIcon className="h-4 w-4"/>
         </Button>
-        <Button type="button" variant="outline" size="icon" className="border-0 bg-muted" onClick={() => copy(value)}>
-          <ClipboardCopyIcon className="h-4 w-4"/>
-        </Button>
+        {/*<Button type="button" variant="outline" size="icon" className="border-0 bg-muted" onClick={() => copy(value)}>*/}
+        {/*  <ClipboardCopyIcon className="h-4 w-4"/>*/}
+        {/*</Button>*/}
       </div>
     </Card>
   )
