5fc91f8f4691c4be0ac42edf1e270fe15a49870b
stasyzon <stasizon@gmail.com>
2024-10-20 14:12:37 +0300

    Add Coolshape component and update dependencies in project files

--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,19 @@
 import ModeToggle from "@/components/themeToggle";
+import {Coolshape} from "coolshapes-react";
+import {Lock} from 'lucide-react';
 
 function Header() {
   return (
     <div className="w-full flex flex-row justify-center items-center">
+      <div className="relative mr-2">
+        <Coolshape
+          type="misc"
+          index={8}
+          noise={true}
+          size={48}
+        />
+        <Lock color="#fff" className="absolute inset-0 m-auto"/>
+      </div>
       <div className="w-full flex flex-col items-start flex-wrap">
         <h4 className="text-xl font-semibold tracking-tight">
           RandPass
