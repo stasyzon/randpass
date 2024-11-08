1c04ec41ab1ecf4ae851e761c14588d06cc701e8
stasyzon <stasizon@gmail.com>
2024-11-08 20:26:30 +0200

    Fix docker build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const createNextIntlPlugin = require('next-intl/plugin');
 const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "standalone"
+};
 
 module.exports = withNextIntl(nextConfig);
